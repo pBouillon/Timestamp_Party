e8c48f6d5655e013721fababdcd3532f04e0193a
Pierre Bouillon <pierre.bouillon@openmailbox.org>
2017-06-23 22:57:17 +0200

    Added Python 3 version

---
+++ Timestamp_Party.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# author: pBouillon - https://github.com/pBouillon
+
+import time
+from time import sleep
+from time import time
+
+def beep():
+    print ('\a')
+
+if __name__ == '__main__':
+    awaited_timestamp = 1500000000
+    ctsp = 0
+    waiting = True
+
+    while (waiting) :
+        ctsp = int(round(time())
+        if ctsp == awaited_timestamp :
+            waiting = False
+        sleep(.3)
+
+    beep ()
+    print ("""\n
+    ************************\n
+         Merry Timestamp !  \n
+            1500000000      \n
+    ************************"
+    \n""")
+    beep ()
