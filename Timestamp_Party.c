41d4edd6b61ab996a9b106cb72e73b4350a0a7cd
Pierre Bouillon <pierre.bouillon@openmailbox.org>
2017-06-23 15:51:39 +0200

    Added C version

---
+++ Timestamp_Party.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+#define     AWAITED_ONE   1500000000
+#define     RING          printf("%c",7) ;
+#define     TIMESTAMP     (unsigned)time(NULL)
+
+int main (int argc, char** argv)
+{
+	int ctsp ;
+
+    for (;;) {
+        ctsp = TIMESTAMP ;
+        if (ctsp == AWAITED_ONE)
+        {
+            break ;
+        }
+        sleep(0.3) ;
+    }
+
+    RING ;
+    printf(
+        "\n%s\n",
+        "************************\n"
+        "     Merry Timestamp !  \n"
+        "        1500000000      \n"
+        "************************"
+    ) ;
+    RING ;
+}
