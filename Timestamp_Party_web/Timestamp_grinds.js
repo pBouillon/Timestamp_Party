975a40149750db5cdd76c152bfc17a563662c8b6
Pierre Bouillon <pierre.bouillon@openmailbox.org>
2017-06-24 14:57:45 +0200

    added web version using JS and HTML/CSS

---
+++ Timestamp_Party_web/Timestamp_grinds.js
@@ -0,0 +1,56 @@
+var awaited = 1500000000
+
+function begin() {
+	var cts_field   = document.getElementById("cts") ;
+	var to_ts_field = document.getElementById("to_ts") ;
+
+	get_date() ;
+
+	/* doesn't run anything if it's more than a day after the event */
+	if ( awaited-get_timestamp() > -86400 ) {
+		countdown(cts_field, to_ts_field) ;
+	}
+	else 
+	{
+		var errmsg = "Too bad, the event ended "+
+						(get_timestamp()-awaited)+
+						" seconds ago" ;
+		cts_field.textContent = errmsg ;
+		to_ts_field.textContent = errmsg ; 
+	}
+}
+
+function countdown(cts_field, to_ts_field) {
+	window.setInterval(function () {
+        var cts     = get_timestamp() ;
+		var to_ts   = awaited - cts
+
+		cts_field.textContent = cts ;
+		to_ts_field.textContent = to_ts ; 
+
+		console.log(cts) ;
+ 
+		if (to_ts <= 0) {
+			party() ;
+		}
+
+	}, 500);
+}
+
+function get_date() {
+	var date = new Date((get_timestamp()) * 1000) ;
+	document.getElementById("date").textContent = "on "+ date ;
+}
+
+function get_timestamp() {
+	return Date.now() / 1000 | 0 ;
+}
+
+function party() {
+	document.body.innerHTML = '' ;
+
+	var party_h1 = document.createElement ("h1") ;
+	party_h1.textContent = "Merry Timestamp !" ;
+	document.body.appendChild (party_h1) ;
+	new Audio('./party.mp3').play()
+}
