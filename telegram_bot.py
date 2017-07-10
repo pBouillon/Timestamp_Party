75e5c0b09e56fa1c0de7d6111debe078a82956b7
Pierre Bouillon <pierre.bouillon@openmailbox.org>
2017-07-10 11:35:12 +0200

    Slight improvement

--- telegram_bot.py
+++ telegram_bot.py
@@ -14,6 +14,23 @@ from time           import sleep
 TOKEN = 'your_token_goes_here'
 
 
+def alert (bot, update):
+    '''Send an alert
+
+    Warn the user the bot is running
+    Send a notification on the awaited Timestamp
+    '''
+    update.message.reply_text (
+        'Aknowledged {}, I\'ll alert you\n'\
+        'Meanwhile, I won\'t be able to perform another action '\
+        '(looks like I need a patch)'
+        .format(update.message.from_user.first_name)
+    )
+    wait4it ()
+    update.message.reply_text (
+        'Merry Timestamp !\nIt reached 1 500 000 000 !'
+    )
+
 def cmd_list (bot, update) :
     msg = 'My commands:                               \n'\
           '/alert_me    -> start the countdown        \n'\
@@ -27,18 +44,8 @@ def display_ctd (bot, update):
     update.message.reply_text (msg)
 
 def start (bot, update):
-    '''Send an alert
-
-    Warn the user the bot is running
-    Send a notification on the awaited Timestamp
-    '''
     update.message.reply_text (
-        'Aknowledged {}, I\'ll alert you'
-        .format(update.message.from_user.first_name)
-    )
-    wait4it ()
-    update.message.reply_text (
-        'Merry Timestamp !\nIt reached 1 500 000 000 !'
+        'Greetings!\nSend me /list to see what I\'m capable of !'
     )
 
 def wait4it ():
@@ -58,9 +65,10 @@ def wait4it ():
 if __name__ == '__main__':
     updater = Updater (TOKEN)
 
-    updater.dispatcher.add_handler(CommandHandler('list',     cmd_list))
+    updater.dispatcher.add_handler(CommandHandler('start',    start))
+    updater.dispatcher.add_handler(CommandHandler('list' ,    cmd_list))
     updater.dispatcher.add_handler(CommandHandler('progress', display_ctd))
-    updater.dispatcher.add_handler(CommandHandler('alert_me', start))
+    updater.dispatcher.add_handler(CommandHandler('alert_me', alert))
 
     updater.start_polling()
     updater.idle()
